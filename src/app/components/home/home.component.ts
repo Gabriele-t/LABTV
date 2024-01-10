75acd5a05c3ae12213f1e831f29a2b5cb2f4d1da
Gabriele Romano <gabriele108@icloud.com>
2024-01-10 02:02:25 +0100

    carousel

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { MovieList } from 'src/app/models/movie.model';
 import { MovieService } from 'src/app/services/movie.service';
 import { environment } from 'src/environments/environment'
+import { SlickCarouselModule } from 'ngx-slick-carousel';
 
 @Component({
   selector: 'app-home',
@@ -12,10 +13,9 @@ export class HomeComponent {
   movieList = new MovieList
   imgSrc = environment.imgSrc
   slickConfig = {
-    slidesToShow: 3,
+    slidesToShow: 2,
     slidesToScroll: 1,
     arrows: true,
-    dots: true,
     infinite: true,
   };
 
