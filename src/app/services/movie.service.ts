c2b8d81de9d677b92f9002e9190e07840ec60a84
Gabriele Romano <gabriele108@icloud.com>
2024-01-12 23:47:48 +0100

    MovieService search

--- src/app/services/movie.service.ts
+++ src/app/services/movie.service.ts
@@ -7,10 +7,19 @@ import { environment } from 'src/environments/environment';
   providedIn: 'root'
 })
 export class MovieService {
+  private apiKey = 'ff7577418c83df0de76681eceaf3c708';
 
   constructor(private http: HttpClient) { }
 
   get() {
     return this.http.get<MovieList>(environment.popularMovie)
+  }
+
+  search(query: string) {
+    const params = {
+      api_key: this.apiKey,
+      query: query
+    };
+    return this.http.get(environment.search, { params });
   }
 }
